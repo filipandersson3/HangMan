9eee72796ba8d5d19f5c111b37fd4331591a98ba
filipandersson3 <60388955+filipandersson3@users.noreply.github.com>
2021-01-15 10:57:39 +0100

    la till en funktion "RandomWord" som används i main

---
+++ src/HangMan.java
@@ -0,0 +1,11 @@
+public class HangMan {
+    public static void main(String[] args) {
+        String word = RandomWord();
+        System.out.println(word);
+    }
+
+    private static String RandomWord() {
+        String word = "hello";
+        return word;
+    }
+}
