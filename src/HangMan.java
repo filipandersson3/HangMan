a029cc369358f637771daf17aabd396bb05def67
filipandersson3 <60388955+filipandersson3@users.noreply.github.com>
2021-01-15 11:17:12 +0100

    fortsatt så att orden ska så småningom kunna läsas från en fil wordList.txt

--- src/HangMan.java
+++ src/HangMan.java
@@ -1,4 +1,9 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Random;
+import java.util.Scanner;
 
 public class HangMan {
     public static void main(String[] args) {
@@ -7,8 +12,18 @@ public class HangMan {
     }
 
     private static String RandomWord() {
+        Scanner input = null;
+        try {
+            input = new Scanner(new File("wordList.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        ArrayList<String> wordList = new ArrayList<>();
+        while (input.hasNext()) {
+            wordList.add(input.nextLine());
+            input.nextLine();
+        }
         Random randNum = new Random();
-        String[] wordList = {"hello","word","sample"};
         String word = wordList[randNum.nextInt(wordList.length)];
         return word;
     }
