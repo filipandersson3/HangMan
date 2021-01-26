524e187bd4f0b204b11e7fe46763e3934463e120
filipandersson3 <60388955+filipandersson3@users.noreply.github.com>
2021-01-26 11:16:27 +0100

    feat: lagt till en funktion som kollar om bokstaven finns och på vilken plats och också i början att programmet skriver _____, om det är fel gissning står det det

--- src/HangMan.java
+++ src/HangMan.java
@@ -8,22 +8,47 @@ public class HangMan {
     public static void main(String[] args) {
         boolean gameEnd = false;
         String word = RandomWord();
+        char[] correctCharList = new char[word.length()];
+        for (int o = 0; o < word.length(); o++) {
+            correctCharList[o] = '_';
+        }
         while (!gameEnd) {
             System.out.println(word);
+            System.out.println(correctCharList);
             String in = ReadInput();
             System.out.println(in);
             boolean isLetter = CheckIfLetter(in);
             if (isLetter) {
                 System.out.println("it's a letter");
+                CheckLetterGuess(word, in);
             } else {
-                if (in.equals(word)) {
-                    System.out.println("you guessed the word!");
-                    gameEnd = true;
-                } else {
-                    System.out.println("wrong guess, you lose.");
-                    gameEnd = true;
+                gameEnd = CheckWordGuess(word, in);
+            }
+        }
+    }
+
+    private static boolean CheckWordGuess(String word, String in) {
+        boolean gameEnd;
+        if (in.equals(word)) {
+            System.out.println("you guessed the word!");
+            gameEnd = true;
+        } else {
+            System.out.println("wrong guess, you lose.");
+            gameEnd = true;
+        }
+        return gameEnd;
+    }
+
+    private static void CheckLetterGuess(String word, String in) {
+        if (word.contains(in)) {
+            System.out.println("at");
+            for (int i = 0; i < word.length(); i++) {
+                if (word.charAt(i) == in.charAt(0)) {
+                    System.out.println("place " + i);
                 }
             }
+        } else {
+            System.out.println("wrong letter");
         }
     }
 
